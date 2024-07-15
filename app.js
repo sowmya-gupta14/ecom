a881c2abd792e77b31c646bebc632e1c66b36a4f
sowmya-gupta14 <sowmya.pvt14@gmail.com>
2024-07-16 01:15:03 +0530

    initialised models

---
+++ app.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const app= express();
+
+const cookieParser= require('cookie-parser');
+const path= require('path');
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    res.send('hey')
+})
+
+app.listen(3000);
