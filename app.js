34b16cad8d18ffa6b2104db22cc06f50ecca1a5a
sowmya-gupta14 <sowmya.pvt14@gmail.com>
2024-07-16 20:59:18 +0530

    added models and routers

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express= require('express');
 const app= express();
-
 const cookieParser= require('cookie-parser');
 const path= require('path');
+const db= require('./config/mongoose-connection')
+const productsRouter=require('./routes/productsRouter');
+const usersRouter=require('./routes/usersRouter');
+const ownersRouter=require('./routes/ownersRouter');
 
 app.set('view engine','ejs');
 app.use(express.json());
@@ -10,8 +13,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    res.send('hey')
-})
+
+
+app.use('/owners',ownersRouter);
+app.use('/users',usersRouter);
+app.use('/products',productsRouter);
 
 app.listen(3000);
